e5f074fb1582a3277e41cdc1d99fbd7298a27c9f
Idham Ganteng <idham16iramap.com>
2024-12-28 19:58:55 +0700

    first commit React Native

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeView from "./src/components/WelcomeView";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topBox}>
+        <Text style={{ fontSize: 20 }}>Hello</Text>
+        <Text style={{ fontSize: 22 }}>Let's Program in</Text>
+        <Text style={{ fontSize: 25 }}>React Native</Text>
+      </View>
+      <WelcomeView />
+      <View style={styles.bottomBox}>
+        <Text style={{ fontSize: 20 }}>Hello</Text>
+        <Text style={{ fontSize: 22 }}>Let's Program in</Text>
+        <Text style={{ fontSize: 25 }}>React Native</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#bbb",
+  },
+
+  topBox: {
+    flexDirection: "column",
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#bbb",
+  },
+
+  bottomBox: {
+    flexDirection: "column",
+    flex: 0.3,
+    justifyContent: "center",
+    backgroundColor: "#ddd",
+    alignItems: "center",
   },
 });
---
+++ src/components/WelcomeView.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function WelcomeView() {
+  return (
+    <View>
+      <Text>Welcome to App building coursse</Text>
+    </View>
+  );
+}
+
+export default WelcomeView;
