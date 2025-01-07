877502422c4bf86321b6ebf466cbb73df8e3f5d0
Idham Ganteng <idham16iramap.com>
2025-01-07 21:16:10 +0700

    update today 7 jan 2025, 21.17 WIB

--- src/components/LoadingErrorComponent.js
+++ src/components/LoadingErrorComponent.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import LottieView from "lottie-react-native";
 
-export default class LoadingErrorComponent extends Component {
-  render() {
+export default function LoadingErrorComponent({ isLoading, error }) {
+  if (isLoading) {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <LottieView
+          source={require("../../assets/jsons/animation.json")}
+          autoPlay
+          loop
+          style={{ width: 130, height: 130 }}
+        />
       </View>
-    )
+    );
   }
+
+  if (error) {
+    return (
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <Text>We have an error</Text>
+      </View>
+    );
+  }
+
+  return null;
 }
